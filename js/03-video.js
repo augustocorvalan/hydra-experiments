3379b14d614ad8a958bdb489b5be4f68f211a784
Augusto Corvalan <augustocorvalan@Augustos-MacBook-Pro.local>
2020-03-29 20:58:05 -0400

    updated video 3

--- js/03-video.js
+++ js/03-video.js
@@ -4,9 +4,21 @@ vid.loop = true;
 vid.src = "/img/eye-1.mp4";
 
 
-osc([3,4,5,8,10], 0.1, 1)
-.kaleid([40,20])
-.rotate(1, 1)
+function range(min, max, step=1) {
+  const arr = [];
+  for (var i=min;i<max;i+=step) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+// osc([3,4,5,8,10], 0.1, 1)
+// .kaleid([40,20])
+// .rotate(1, 1)
+// .out(o1)
+voronoi(range(0.5,2,0.25).fast(0.5),1,[3,4])
+.pixelate(4,range(1,10))
+//.rotate(() => Math.sin(time),0.05)
 .out(o1)
 
 
@@ -18,13 +30,14 @@ s0.init(
 );
 
 src(s0)
-.scrollX(0, 0.05)
-.repeat([2,4])
-.color([0,2,0.5],[1,2],[1,0,0.5])
+.scrollX(0.5, 0.25)
+.repeat([2,4].fast(0.25))
+//.color([0,2,0.5],[1,2],[1,0,0.5])
+//.saturate(0.75)
 .out(o4)
 
 src(o4)
-.modulate(o1)
+.modulateRepeat(o1,[1,5].fast(0.75))
 .out(o0);
 
 //render()
